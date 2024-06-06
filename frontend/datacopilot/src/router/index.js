1aefd2f08aaaf5bf03eaf3212196a3cfce0cb942
ChoCoo1 <1753373709@qq.com>
2024-06-06 19:40:27 +0800

    创建基础vue项目和后端项目

--- frontend/datacopilot/src/router/index.js
+++ frontend/datacopilot/src/router/index.js
@@ -1,20 +1,36 @@
 import {createRouter, createWebHistory} from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const routes = [
     {
         path: '/',
         name: 'home',
-        component: HomeView
+        component: () => import('../views/HomeView.vue')
     },
     {
-        path: '/about',
-        name: 'about',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-    }
+        path: '/login',
+        name: 'login',
+        component: () => import('../views/LoginView.vue')
+    },
+    {
+        path: '/register',
+        name: 'register',
+        component: () => import('../views/RegisterView.vue')
+    },
+    {
+        path: '/query',
+        name: 'query',
+        component: () => import('../views/QueryView.vue')
+    },
+    {
+        path: '/database',
+        name: 'database',
+        component: () => import('../views/DatabaseView.vue')
+    },
+    {
+        path: '/user',
+        name: 'user',
+        component: () => import('../views/UserView.vue')
+    },
 ]
 
 const router = createRouter({
